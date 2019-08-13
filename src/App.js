6b7483aa306ffc20ea81f67f5438f445b46c4b45
Andre Garcia <Andre.Garcia@la.logicalis.com>
2019-08-13 11:55:59 -0300

    Cleaned Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './Header'
+import Generos from './Generos'
+import NovoGenero from './NovoGenero'
+import EditarGenero from './EditarGenero'
+import Series from './Series'
+import NovaSerie from './NovaSerie'
+import InfoSerie from './InfoSerie'
+
+const Home = () => {
+  return <h1>Home</h1>
 }
 
-export default App;
+function App() {  
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/generos' exact component={Generos} />
+          <Route path='/generos/novo' exact component={NovoGenero} />
+          <Route path='/generos/:id' exact component={EditarGenero} />
+          <Route path='/series' exact component={Series} />
+          <Route path='/series/novo' exact component={NovaSerie} />
+          <Route path='/series/:id' exact component={InfoSerie} />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App

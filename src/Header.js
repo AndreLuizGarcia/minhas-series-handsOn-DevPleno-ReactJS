4fe100a74abeb49809b7cf1207d78563054dcd15
Andre Garcia <Andre.Garcia@la.logicalis.com>
2019-08-13 12:01:06 -0300

    Header Component

---
+++ src/Header.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler
+} from 'reactstrap'
+
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  const [open, setOpen] = useState(false)
+
+  const toggle = () => {
+    setOpen(!open)
+  }
+
+  return(
+    <Navbar color='light' light expand='md'>
+      <div className='container'>
+        <NavbarBrand tag={Link} to='/' >Minhas Séries</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={open} navbar>
+          <Nav className='ml-auto' navbar>
+            <NavItem>
+              <NavLink tag={Link} to='/series'>Séries</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to='/generos'>Generos</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </div>
+    </Navbar>
+  )
+}
+
+export default Header
